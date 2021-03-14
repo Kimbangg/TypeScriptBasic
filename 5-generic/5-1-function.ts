33e779dffc06600e4a32b7ae8aa7d6809a39cf8e
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-03-14 12:49:40 +0900

    # How to use Generic in function

---
+++ 5-generic/5-1-function.ts
@@ -0,0 +1,30 @@
+{
+  // 타입 별로 만드는건 무의미.
+  function checkNotNullBad(arg: number | null): number {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  // 타입이 미정이라 좋은 케이스가 아님
+  function checkNotNullAnyBad(arg: any | null): any {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+    return arg;
+  }
+
+  const number = checkNotNull(123);
+  const boal: boolean = checkNotNull(true);
+  console.log(number);
+  console.log(boal);
+  checkNotNull(null);
+}
